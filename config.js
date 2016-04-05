042a5c52d3740129245ce92e6a3a4cf31ecd5138
mpnaidu26 <pmolakal@hawk.iit.edu>
2016-04-05 12:51:39 -0500

    REST API

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://admin:1@ds064278.mlab.com:64278/confusion'
+}
---
+++ routes/promoRouter.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var promoRouter = express.Router();
+var Promos = require('../models/promotions');
+var Verify = require('./verify');
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.get(Verify.verifyOrdinaryUser, function(req,res,next){
+    Promos.find({}, function(err, promotion){
+        if(err) throw err;
+        res.json(promotion);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next){
+    Promos.create(req.body, function(err, promotion){
+        if(err) throw err;
+        console.log('Promotion added!');
+        var id = promotion._id;
+        
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the promotion with id: ' + id);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next){
+    Promos.remove({}, function(err, resp){
+        if(err) throw err;
+        res.json(resp);
+    });
+});
+
+promoRouter.route('/:promoId')
+.get(Verify.verifyOrdinaryUser, function(req,res,next){
+    Promos.findById(req.params.promoId, function(err, promotion){
+        if(err) throw err;
+        res.json(promotion);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next){
+    Promos.findByIdAndUpdate(req.params.promoId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next){
+    Promos.findByIdAndRemove(req.params.promoId, function (err, resp) {        
+        if(err) throw err;
+        res.json(resp);
+    });
+});
+    
+module.exports = promoRouter;
